6c8b5ff0b18a2c0920f16a690fde429b11a8250b
kolya <horov.kolya@yandex.ru>
2022-11-30 23:22:47 +0300

    26 task completed

---
+++ src/ru/mirea/task26/ThirdTask.java
@@ -0,0 +1,34 @@
+package ru.mirea.task26;
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class ThirdTask {
+    public static void main(String[] args) {
+        Hashtable table = new Hashtable(5,(float) 0.5);
+        table.put(235,"firstAdd");
+        table.put(52,"secondAdd");
+        table.put(12897,"thirdAdd");
+
+        System.out.println("Введите значение ключа, элемент которого следует вернуть");
+        Scanner scanner = new Scanner(System.in);
+        int key=scanner.nextInt();
+        String flag= (String) table.get(key);
+        if(flag!=null){
+            System.out.println(key+" "+flag);
+        }else {
+            System.out.println("Введенный вами ключ отсутствует в таблице");
+        }
+
+        System.out.println("Введите значение ключа, элемент которого следует удалить");
+        key=scanner.nextInt();
+        flag= (String) table.get(key);
+        if(flag!=null){
+            table.remove(key);
+            System.out.println(table);
+        }else {
+            System.out.println("Введенный вами ключ отсутствует в таблице");
+        }
+
+    }
+}
